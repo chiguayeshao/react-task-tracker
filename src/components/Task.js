12eca6eaa7282a0c34590206a7df9d434686e949
chiguayeshao <chiguayeshao@gmail.com>
2021-06-23 16:03:42 +0800

    add task

---
+++ src/components/Task.js
@@ -0,0 +1,11 @@
+// 将task单独提取
+const Task = ({ task }) => {
+    return (
+        <div className='task'>
+            <h3>{task.text}</h3>
+            <p>{task.day}</p>
+        </div>
+    )
+}
+
+export default Task
--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,8 +1,9 @@
+import Task from "./Task"
 // 获取传入的tasks内容
 const Tasks = ({ tasks }) => {
     return (
         <>
-            {tasks.map((task) => (<h3 key={task.id}>{task.text}</h3>))}
+            {tasks.map((task) => (<Task key={task.id} task={task} />))}
         </>
     )
 }
