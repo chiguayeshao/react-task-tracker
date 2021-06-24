d7741866bdf78e15ea620549182a6fc6b0a76482
chiguayeshao <chiguayeshao@gmail.com>
2021-06-24 14:10:10 +0800

    add delete task fucntion

--- src/App.js
+++ src/App.js
@@ -9,37 +9,42 @@ const App = () => {
     [
       {
         id: 1,
-        text: 'Doctor Appoinment',
+        text: 'Doctor Appoinment1',
         day: 'Feb 5th at 2:30pm',
         reminder: true,
       },
       {
         id: 2,
-        text: 'Doctor Appoinment',
+        text: 'Doctor Appoinment2',
         day: 'Feb 5th at 2:30pm',
         reminder: true,
       },
       {
         id: 3,
-        text: 'Doctor Appoinment',
+        text: 'Doctor Appoinment3',
         day: 'Feb 5th at 2:30pm',
         reminder: true,
       },
       {
         id: 4,
-        text: 'Doctor Appoinment',
+        text: 'Doctor Appoinment4',
         day: 'Feb 5th at 2:30pm',
         reminder: true,
       },
     ]
   )
 
+  //delete task
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id))
+  }
+
   return (
     <div className='container'>
       {/* 调用header 也可以在此给title赋值*/}
       <Header />
       {/* 将tasks内容传入Tasks */}
-      <Tasks tasks={tasks} />
+      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} /> : 'fuck'}
     </div>
   );
 }
--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,9 +1,9 @@
 import Task from "./Task"
 // 获取传入的tasks内容
-const Tasks = ({ tasks }) => {
+const Tasks = ({ tasks, onDelete }) => {
     return (
         <>
-            {tasks.map((task) => (<Task key={task.id} task={task} />))}
+            {tasks.map((task) => (<Task key={task.id} task={task} onDelete={onDelete} />))}
         </>
     )
 }
