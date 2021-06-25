8d54ae4d319b33fdc2653dd4f270d96a558877d2
chiguayeshao <chiguayeshao@gmail.com>
2021-06-25 14:27:45 +0800

    add post request

--- src/App.js
+++ src/App.js
@@ -29,10 +29,16 @@ const App = () => {
   }
 
   //add task
-  const addTask = (task) => {
-    const id = Math.floor(Math.random() * 10000) + 1
-    const newTask = { id, ...task }
-    setTasks([...tasks, newTask])
+  const addTask = async (task) => {
+
+    const res = await fetch('http://localhost:5555/tasks', { method: 'POST', headers: { 'Content-type': 'application/json' }, body: JSON.stringify(task) })
+
+    const data = await res.json()
+    setTasks([...tasks, data])
+
+    // const id = Math.floor(Math.random() * 10000) + 1
+    // const newTask = { id, ...task }
+    // setTasks([...tasks, newTask])
   }
 
   //delete task
